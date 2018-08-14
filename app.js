927cff38d30a69e102d872c9a65d84f7eb54b9b0
Carlos Saito <c.saito.style@gmail.com>
2018-08-14 13:20:05 +0200

    Send a response when something fails

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const express = require('express')
 const path = require('path')
 const rp = require('request-promise')
 const bunyan = require('bunyan')
+const packageFile = require('./package.json')
 
-/// const log = require('./server/log')
+// Export a logger
+const log =  bunyan.createLogger({
+  name: 'build-monitor',
+  app: packageFile.name
+})
+
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000
 const log = bunyan.createLogger({
   name: 'build-monitor'
@@ -48,48 +54,48 @@ async function jenkinsApi (url, username, token) {
 }
 
 async function getStatusFromJenkins (req, res) {
-  const socialNames = [
-    'social-develop',
-    'social-master',
-    'social-features'
-  ]
-  const jenkinsKTH = await jenkinsApi(`https://jenkins.sys.kth.se/api/json`, process.env.JENKINS_USER, process.env.JENKINS_TOKEN)
-  const socialBuilds = jenkinsKTH.filter(j => socialNames.includes(j.name))
+  try {
+    const socialNames = [
+      'social-develop',
+      'social-master',
+      'social-features'
+    ]
+    const jenkinsKTH = await jenkinsApi(`https://jenkins.sys.kth.se/api/json`, process.env.JENKINS_TOKEN)
+    const socialBuilds = jenkinsKTH.filter(j => socialNames.includes(j.name))
+    const lmsNames = [
+      'lms-export-results',
+      'lms-sync-users',
+      'lms-sync-courses',
+      'lms-api'
+    ]
+    const buildKTH = await jenkinsApi(`https://build.sys.kth.se/api/json`, process.env.BUILD_TOKEN)
+    const lmsBuilds = buildKTH.filter(j => lmsNames.includes(j.name))
 
-  const lmsNames = [
-    'lms-export-results',
-    'lms-sync-users',
-    'lms-sync-courses',
-    'lms-api'
-  ]
-  const buildKTH = await jenkinsApi(`https://build.sys.kth.se/api/json`, process.env.BUILD_USER, process.env.BUILD_TOKEN)
-  const lmsBuilds = buildKTH.filter(j => lmsNames.includes(j.name))
+    const filteredJobs = [...socialBuilds, ...lmsBuilds]
 
-  const filteredJobs = [...socialBuilds, ...lmsBuilds]
+    const statusLib = {
+      blue: 'alert-success',
+      red: 'alert-danger',
+      yellow: 'alert-info',
+      blue_anime: 'alert-success progress-bar-striped progress-bar-animated',
+      red_anime: 'alert-danger progress-bar-striped progress-bar-animated',
+      yellow_anime: 'alert-info progress-bar-striped progress-bar-animated'
+    }
 
-  const statusLib = {
-    blue: 'alert-success',
-    red: 'alert-danger',
-    yellow: 'alert-info',
-    blue_anime: 'alert-success progress-bar-striped progress-bar-animated',
-    red_anime: 'alert-danger progress-bar-striped progress-bar-animated',
-    yellow_anime: 'alert-info progress-bar-striped progress-bar-animated'
-  }
+    const stringDiv = filteredJobs
+      .map(build =>
+        `<div
+           aria-live="polite"
+           role="alert"
+           class="alert ${statusLib[build.color]}"
+         >
+           <p><b>${build.name}</b> <a href="${build.url}">check for more</a></p>
+         </div>`)
+      .join('')
 
-  const stringDiv = filteredJobs
-    .map(build =>
-      `<div
-         aria-live="polite"
-         role="alert"
-         class="alert ${statusLib[build.color]}"
-       >
-         <p><b>${build.name}</b> <a href="${build.url}">check for more</a></p>
-       </div>`)
-    .join('')
-
-  return res.send(`
-    <html>
-      <head>
+    return res.send(`
+      <html>
+        <head>
         <meta charset=utf-8>
         <title>Build status</title>
         <meta http-equiv="refresh" content="10">
@@ -97,7 +103,18 @@ async function getStatusFromJenkins (req, res) {
         <link rel="stylesheet" href="/app/build-monitor/kth-style/css/kth-bootstrap.css">
         <h2>Monitor builds for Social and LMS projects</h2>
         ${stringDiv}
-  `)
+    `)
+  } catch (e) {
+    log.error('Error in build-monitor', e)
+    return res.send(`
+      <html>
+      <head>
+      <meta charset=utf-8>
+      <title>Build status failing</title>
+      <meta http-equiv="refresh" content="10">
+      <div>Build status failing. Refreshing in 10 seconds. Check the logs...</div>
+    `)
+  }
 }
 
 server.get(prefix + '/test', getStatusFromJenkins)
