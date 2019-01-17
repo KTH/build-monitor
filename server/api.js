04f0f0e8a8da95d78dd7cdb9336eeff75add6b15
Carlos Saito <exacs@users.noreply.github.com>
2019-01-17 12:36:10 +0100

    Disable "Canvas Import Errors" temporary

--- server/api.js
+++ server/api.js
@@ -1,16 +1,7 @@
 const express = require('express')
-const papaparse = require('papaparse')
 const apiRouter = express.Router()
 
 const getStatusFromJenkins = require('./lib/jenkinsStatus')
-const { getCanvasLogs, renewCanvasLogs } = require('./lib/canvasImportErrors')
-
-const logToObject = (log) => {
-  const lines = log.split('\n').slice(1).join('\n')
-  return papaparse.parse(lines, {
-    header: true
-  }).data
-}
 
 apiRouter.get('/', async (req, res) => {
   try {
@@ -23,20 +14,9 @@ apiRouter.get('/', async (req, res) => {
 })
 
 apiRouter.get('/import-errors', async (req, res) => {
-  try {
-    const cachedLog = await getCanvasLogs({log: req.log})
-    const status = await renewCanvasLogs(cachedLog, {log: req.log})
-
-    res.send({
-      lastUpdate: parseInt(cachedLog.timeStamp, 10),
-      nextUpdate: parseInt(cachedLog.timeStamp, 10) + parseInt(process.env.LOG_TTL, 10),
-      log: logToObject(cachedLog.log),
-      status
-    })
-  } catch (e) {
-    req.log.error(e)
-    res.status(500).send({ status: 500 })
-  }
+  res.status(404).send({
+    message: 'Sorry, we are disabling temporary this feature until further notice'
+  })
 })
 
 module.exports = apiRouter
